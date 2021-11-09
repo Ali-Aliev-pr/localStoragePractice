a4ada7782fd1e70aa327404585837a567b4df969
aliali2003 <aliali2003@MacBook-Air-aliali2003.local>
2021-11-09 16:51:47 +0300

    done method Update

---
+++ index.py
@@ -0,0 +1,6 @@
+a = []
+
+for i in range(0,10000):
+    a.append(i)
+
+print(a)
--- script.js
+++ script.js
@@ -10,18 +10,49 @@ btnClear.addEventListener('click', clearStorage);
 const btnSave = document.querySelector('#save');
 btnSave.addEventListener('click', saveItem);
 
+let output = document.getElementById('output');
+
 // const delbtn = document.getElementById('del');
 // delbtn.addEventListener('click', )
 
 const inputKey = document.getElementById('key');
 const inputValue = document.getElementById('value');
    
-function updateTable(){}
+function updateTable(){
+    if (localStorage.length == 0) {
+        console.log(emptyHeader);
+    } else {
+        output.innerHTML = ``
+        for(let i = 0; i < localStorage.length; i++) {
+            let column = document.createElement('tr');
+            let idtext = document.createElement('td');
+            let keytext = document.createElement('td');
+            let valuetext = document.createElement('td');
+            let key = localStorage.key(i)
 
-function getStorage(){}
+            idtext.innerText = i+1
+            keytext.innerText = key
+            valuetext.innerText = localStorage.getItem(key);
+            column.appendChild(idtext);
+            column.appendChild(keytext);
+            column.appendChild(valuetext);
+            output.appendChild(column);
+            console.log(localStorage.length);
+        }
+    }
+}
+
+// idtext.innerText = localStorage.key(i);
+
+
+function getStorage(){
+    let a = storage.value[0]
+    console.log(a);
+}
 
 function saveItem(){
     localStorage.setItem(inputKey.value, inputValue.value)
+    updateTable()
 }
 
 function deleteItem(key){}
